92f9952bdc13d4368c8bb55de03bff6909561b09
vincent <vincentcharlebois@gmail.com>
2024-07-03 23:12:31 -0400

    ✔️

---
+++ src/lib/Outro/Confirmation.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { account, intro } from '../../store';
+
+    function premintMore() {
+        intro.set(0);
+    }
+</script>
+  
+
+    <div>
+      <h2>Submission report:</h2>
+      <p>Thank you for using our premint tool!</p>
+      <p>
+        <a href="https://zora.co/{$account.address}">Check your profile</a>, maybe you'll find the NFT there.<br>Alternatively, you can <button on:click={premintMore}>premint some more</button>.
+      </p>
+    </div>
+  
