e76fc4d4b6c3893ae6a9f45e0607a5d5f1f78b6e
vincent <vincentcharlebois@gmail.com>
2024-07-03 23:45:08 -0400

    🚧

--- src/lib/Wallet.svelte
+++ src/lib/Wallet.svelte
@@ -31,16 +31,6 @@
 
 <style>
 	button {
-		background-color: #4CAF50;
-		border: none;
-		color: white;
-		padding: 15px 32px;
-		text-align: center;
-		text-decoration: none;
-		display: inline-block;
-		font-size: 16px;
-		margin: 4px 2px;
-		cursor: pointer;
-		margin: 0.5rem;
+		background-color: #286e2a;
 	}
 </style>
