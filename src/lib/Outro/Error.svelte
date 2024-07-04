297a77eb328e72d01838549d947980ce0c728701
vincent <vincentcharlebois@gmail.com>
2024-07-03 23:12:42 -0400

    ⚠️

---
+++ src/lib/Outro/Error.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { intro } from '../../store';
+
+    function tryAgain() {
+        intro.set(3);
+    }
+</script>
+    <div>
+      <h2>Error Occurred</h2>
+      <p>There was an error during the signing or submission process. Please 
+        <button on:click={tryAgain}>try again.</button></p>
+    </div>
+  
